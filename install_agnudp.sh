7d8c2200b1a5f9744478f26aea44da252b538055
Khaled AGN <68344344+khaledagn@users.noreply.github.com>
2023-05-11 01:32:04 +0000

    Update install_agnudp.sh

--- install_agnudp.sh
+++ install_agnudp.sh
@@ -1006,6 +1006,16 @@ start_services() {
 	echo "Starting AGN-UDP"
 	iptables -t nat -A PREROUTING -i $(ip -4 route ls|grep default|grep -Po '(?<=dev )(\S+)'|head -1) -p udp --dport 10000:50000 -j DNAT --to-destination :$UDP_PORT
 	ip6tables -t nat -A PREROUTING -i $(ip -4 route ls|grep default|grep -Po '(?<=dev )(\S+)'|head -1) -p udp --dport 10000:50000 -j DNAT --to-destination :$UDP_PORT
+	sysctl net.ipv4.conf.all.rp_filter=0
+	sysctl net.ipv4.conf.$(ip -4 route ls|grep default|grep -Po '(?<=dev )(\S+)'|head -1).rp_filter=0 
+	echo "net.ipv4.ip_forward = 1
+	net.ipv4.conf.all.rp_filter=0
+	net.ipv4.conf.$(ip -4 route ls|grep default|grep -Po '(?<=dev )(\S+)'|head -1).rp_filter=0" > /etc/sysctl.conf  
+	sysctl -p
+	echo iptables-persistent iptables-persistent/autosave_v4 boolean true | sudo debconf-set-selections 
+        echo iptables-persistent iptables-persistent/autosave_v6 boolean true | sudo debconf-set-selections 
+        iptables-save > /etc/iptables_rules.v4
+        ip6tables-save > /etc/iptables_rules.v6
 	systemctl enable hysteria-server.service
 	systemctl start hysteria-server.service	
 }
