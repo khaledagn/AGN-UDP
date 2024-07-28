2fda11511c83b17c98c2de56a8bab4336de1c7e0
Khaled AGN <68344344+khaledagn@users.noreply.github.com>
2024-07-28 12:21:01 +0100

    Update agnudp_manager.sh

--- agnudp_manager.sh
+++ agnudp_manager.sh
@@ -21,116 +21,116 @@ update_userpass_config() {
 }
 
 add_user() {
-    echo "Enter username:"
+    echo -e "\n\e[1;34mEnter username:\e[0m"
     read -r username
-    echo "Enter password:"
+    echo -e "\e[1;34mEnter password:\e[0m"
     read -r password
     sqlite3 "$USER_DB" "INSERT INTO users (username, password) VALUES ('$username', '$password');"
     if [[ $? -eq 0 ]]; then
-        echo "User $username added successfully."
+        echo -e "\e[1;32mUser $username added successfully.\e[0m"
         update_userpass_config
         restart_server
     else
-        echo "Error: Failed to add user $username."
+        echo -e "\e[1;31mError: Failed to add user $username.\e[0m"
     fi
 }
 
 edit_user() {
-    echo "Enter username to edit:"
+    echo -e "\n\e[1;34mEnter username to edit:\e[0m"
     read -r username
-    echo "Enter new password:"
+    echo -e "\e[1;34mEnter new password:\e[0m"
     read -r password
     sqlite3 "$USER_DB" "UPDATE users SET password = '$password' WHERE username = '$username';"
     if [[ $? -eq 0 ]]; then
-        echo "User $username updated successfully."
+        echo -e "\e[1;32mUser $username updated successfully.\e[0m"
         update_userpass_config
         restart_server
     else
-        echo "Error: Failed to update user $username."
+        echo -e "\e[1;31mError: Failed to update user $username.\e[0m"
     fi
 }
 
 delete_user() {
-    echo "Enter username to delete:"
+    echo -e "\n\e[1;34mEnter username to delete:\e[0m"
     read -r username
     sqlite3 "$USER_DB" "DELETE FROM users WHERE username = '$username';"
     if [[ $? -eq 0 ]]; then
-        echo "User $username deleted successfully."
+        echo -e "\e[1;32mUser $username deleted successfully.\e[0m"
         update_userpass_config
         restart_server
     else
-        echo "Error: Failed to delete user $username."
+        echo -e "\e[1;31mError: Failed to delete user $username.\e[0m"
     fi
 }
 
 show_users() {
-    echo "Current users:"
+    echo -e "\n\e[1;34mCurrent users:\e[0m"
     sqlite3 "$USER_DB" "SELECT username FROM users;"
 }
 
 change_domain() {
-    echo "Enter new domain:"
+    echo -e "\n\e[1;34mEnter new domain:\e[0m"
     read -r domain
     jq ".server = \"$domain\"" "$CONFIG_FILE" > "${CONFIG_FILE}.tmp" && mv "${CONFIG_FILE}.tmp" "$CONFIG_FILE"
-    echo "Domain changed to $domain successfully."
+    echo -e "\e[1;32mDomain changed to $domain successfully.\e[0m"
     restart_server
 }
 
 change_obfs() {
-    echo "Enter new obfuscation string:"
+    echo -e "\n\e[1;34mEnter new obfuscation string:\e[0m"
     read -r obfs
     jq ".obfs.password = \"$obfs\"" "$CONFIG_FILE" > "${CONFIG_FILE}.tmp" && mv "${CONFIG_FILE}.tmp" "$CONFIG_FILE"
-    echo "Obfuscation string changed to $obfs successfully."
+    echo -e "\e[1;32mObfuscation string changed to $obfs successfully.\e[0m"
     restart_server
 }
 
 change_up_speed() {
-    echo "Enter new upload speed (Mbps):"
+    echo -e "\n\e[1;34mEnter new upload speed (Mbps):\e[0m"
     read -r up_speed
     jq ".up_mbps = $up_speed" "$CONFIG_FILE" > "${CONFIG_FILE}.tmp" && mv "${CONFIG_FILE}.tmp" "$CONFIG_FILE"
     jq ".up = \"$up_speed Mbps\"" "$CONFIG_FILE" > "${CONFIG_FILE}.tmp" && mv "${CONFIG_FILE}.tmp" "$CONFIG_FILE"
-    echo "Upload speed changed to $up_speed Mbps successfully."
+    echo -e "\e[1;32mUpload speed changed to $up_speed Mbps successfully.\e[0m"
     restart_server
 }
 
 change_down_speed() {
-    echo "Enter new download speed (Mbps):"
+    echo -e "\n\e[1;34mEnter new download speed (Mbps):\e[0m"
     read -r down_speed
     jq ".down_mbps = $down_speed" "$CONFIG_FILE" > "${CONFIG_FILE}.tmp" && mv "${CONFIG_FILE}.tmp" "$CONFIG_FILE"
     jq ".down = \"$down_speed Mbps\"" "$CONFIG_FILE" > "${CONFIG_FILE}.tmp" && mv "${CONFIG_FILE}.tmp" "$CONFIG_FILE"
-    echo "Download speed changed to $down_speed Mbps successfully."
+    echo -e "\e[1;32mDownload speed changed to $down_speed Mbps successfully.\e[0m"
     restart_server
 }
 
 restart_server() {
     systemctl restart hysteria-server
-    echo "Server restarted successfully."
+    echo -e "\e[1;32mServer restarted successfully.\e[0m"
 }
 
 uninstall_server() {
-    echo "Uninstalling AGN-UDP server..."
+    echo -e "\n\e[1;34mUninstalling AGN-UDP server...\e[0m"
     systemctl stop hysteria-server
     systemctl disable hysteria-server
     rm -f "$SYSTEMD_SERVICE"
     systemctl daemon-reload
     rm -rf "$CONFIG_DIR"
     rm -f /usr/local/bin/hysteria
-    echo "AGN-UDP server uninstalled successfully."
+    echo -e "\e[1;32mAGN-UDP server uninstalled successfully.\e[0m"
 }
 
 show_banner() {
-    echo "---------------------------------------------"
+    echo -e "\e[1;36m---------------------------------------------"
     echo " AGNUDP Manager"
     echo " (c) 2023 Khaled AGN"
     echo " Telegram: @khaledagn"
-    echo "---------------------------------------------"
+    echo "---------------------------------------------\e[0m"
 }
 
 show_menu() {
-    echo "----------------------------"
+    echo -e "\e[1;36m----------------------------"
     echo " AGNUDP Manager"
-    echo "----------------------------"
-    echo "1. Add new user"
+    echo -e "----------------------------\e[0m"
+    echo -e "\e[1;32m1. Add new user"
     echo "2. Edit user password"
     echo "3. Delete user"
     echo "4. Show users"
@@ -140,9 +140,9 @@ show_menu() {
     echo "8. Change download speed"
     echo "9. Restart server"
     echo "10. Uninstall server"
-    echo "11. Exit"
-    echo "----------------------------"
-    echo "Enter your choice: "
+    echo -e "11. Exit\e[0m"
+    echo -e "\e[1;36m----------------------------"
+    echo -e "Enter your choice: \e[0m"
 }
 
 show_banner
@@ -161,6 +161,6 @@ while true; do
         9) restart_server ;;
         10) uninstall_server; exit 0 ;;
         11) exit 0 ;;
-        *) echo "Invalid choice. Please try again." ;;
+        *) echo -e "\e[1;31mInvalid choice. Please try again.\e[0m" ;;
     esac
 done
